d0a9d0c15ec28ea84950acfe2fa298be64092d32
Abel Calluaud <abel0b@protonmail.com>
2020-05-31 12:02:03 +0200

    install powershell on linux

--- config/ubuntu/config.sh
+++ config/ubuntu/config.sh
@@ -1,4 +1,12 @@
 function sync {
+    if ! command -v pwsh > /dev/null; then
+        sudo wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -d | cut -d" " -f2)/packages-microsoft-prod.deb -O $CACHE_PATH/packages-microsoft-prod.deb
+        sudo dpkg -i $CACHE_PATH/packages-microsoft-prod.deb
+    fi
+
     sudo apt-get update -y
     sudo apt-get install -y --ignore-missing $(cat pkglist.txt)
+    if ! command -v pwsh > /dev/null; then
+        sudo apt-get install -y powershell
+    fi
 }
